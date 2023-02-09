58272e66bf933d813f85c90485bdca5ada6c2cbe
marcelamourino <marcela.mourino@hotmail.com>
2023-02-09 13:56:22 -0300

    Primer Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let toggle=document.getElementById('toggle');
+let label_toggle=document.getElementById('label_toggle');
+toggle.addEventListener('change',(Event)=>{
+    let checked=Event.target.checked;
+    document.body.classList.toggle('dark');
+    if (checked==true){
+     label_toggle.innerHTML='<i class="fa-solid fa-sun"></i>'
+     label_toggle.style.color="yellow";
+    }else{
+        label_toggle.innerHTML='<i class="fa-solid fa-moon"></i>';
+        label_toggle.style.color="rebeccapurple";
+    }
+})
